6a4b134a6b87e13d5bfd08f784cca569f0c1eed2
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-08-03 09:38:19 +0100

    feat: add first branch of code to js of project

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const rockButton = document.querySelector('#rock-button')
+const paperButton = document.querySelector('#paper-button')
+const scissorsButton = document.querySelector('#scissors-button')
+
+const newGameButton = document.querySelector('#new-game-button')
+
+
+function handler(event) {
+    console.log(event.target.textContent)
+}
+
+rockButton.addEventListener("click", handler);
+
+paperButton.addEventListener("click", handler);
+
+scissorsButton.addEventListener("click", handler);
+
+newGameButton.addEventListener('click', handler);
