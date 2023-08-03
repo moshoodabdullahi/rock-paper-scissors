4934322f5fbf82ca58a86f7c8d98333e13fd45c8
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-08-03 09:55:49 +0100

    feat: fix eslint error

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
-const rockButton = document.querySelector('#rock-button')
-const paperButton = document.querySelector('#paper-button')
-const scissorsButton = document.querySelector('#scissors-button')
+const rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
 
-const newGameButton = document.querySelector('#new-game-button')
-
+const newGameButton = document.querySelector('#new-game-button');
 
 function handler(event) {
-    console.log(event.target.textContent)
+  console.log(event.target.textContent);
 }
 
-rockButton.addEventListener("click", handler);
+rockButton.addEventListener('click', handler);
 
-paperButton.addEventListener("click", handler);
+paperButton.addEventListener('click', handler);
 
-scissorsButton.addEventListener("click", handler);
+scissorsButton.addEventListener('click', handler);
 
 newGameButton.addEventListener('click', handler);
