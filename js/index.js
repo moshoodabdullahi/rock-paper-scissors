ddfe680bc4fb6cf0306bd86a6aba11c03836871b
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-08-03 12:45:59 +0100

    feat: add code to index.js so the project can return value randomly

--- js/index.js
+++ js/index.js
@@ -4,14 +4,27 @@ const scissorsButton = document.querySelector('#scissors-button');
 
 const newGameButton = document.querySelector('#new-game-button');
 
-function handler(event) {
+const getComputerChoice = () => {
+  const choice = ['Rock', 'Paper', 'Scissors'];
+  const random = Math.floor(Math.random() * choice.length);
+  return choice[random];
+};
+
+function playGame(event) {
+  console.log(event.target.textContent);
+  const playerSelection = event.target.textContent;
+  const computerSelection = getComputerChoice();
+  console.info('Player Chooses:', playerSelection, 'Computer Chooses:', computerSelection);
+}
+
+rockButton.addEventListener('click', playGame);
+
+paperButton.addEventListener('click', playGame);
+
+scissorsButton.addEventListener('click', playGame);
+
+function startNewGame(event) {
   console.log(event.target.textContent);
 }
 
-rockButton.addEventListener('click', handler);
-
-paperButton.addEventListener('click', handler);
-
-scissorsButton.addEventListener('click', handler);
-
-newGameButton.addEventListener('click', handler);
+newGameButton.addEventListener('click', startNewGame);
