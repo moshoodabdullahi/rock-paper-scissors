b41e5ed4e52b989e5b8b610a6b58f00dd474f9fb
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-08-04 07:17:00 +0100

    feat: add if else statement to project to make decisions

--- js/index.js
+++ js/index.js
@@ -10,11 +10,30 @@ const getComputerChoice = () => {
   return choice[random];
 };
 
+const calculateGame = (computerSelection, playerSelection) => {
+  if (computerSelection === 'Rock' && playerSelection === 'Paper') {
+    console.log('You Win!');
+  } else if (computerSelection === 'Scissors' && playerSelection === 'Rock') {
+    console.log('You Win!');
+  } else if (computerSelection === 'Paper' && playerSelection === 'Scissors') {
+    console.log('You Win!');
+  } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
+    console.log('You Lose!');
+  } else if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
+    console.log('You Lose!');
+  } else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
+    console.log('You Lose!');
+  } else {
+    console.log('Draw');
+  }
+};
+
 function playGame(event) {
   console.log(event.target.textContent);
   const playerSelection = event.target.textContent;
   const computerSelection = getComputerChoice();
   console.info('Player Chooses:', playerSelection, 'Computer Chooses:', computerSelection);
+  calculateGame(computerSelection, playerSelection);
 }
 
 rockButton.addEventListener('click', playGame);
