2d5954709b7c333d6673b3465cd41004e337d059
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-08-22 12:23:00 +0100

    feat: made the new game button reset project UI

--- js/index.js
+++ js/index.js
@@ -21,24 +21,31 @@ const getComputerChoice = () => {
 const calculateGame = (computerSelection, playerSelection) => {
   if (computerSelection === 'Rock' && playerSelection === 'Paper') {
     console.log('You Win!');
+    gameOutcomeElement.textContent = 'You Win!';
     playerScore += 1;
   } else if (computerSelection === 'Scissors' && playerSelection === 'Rock') {
     console.log('You Win!');
+    gameOutcomeElement.textContent = 'You Win!';
     playerScore += 1;
   } else if (computerSelection === 'Paper' && playerSelection === 'Scissors') {
     console.log('You Win!');
+    gameOutcomeElement.textContent = 'You Win!';
     playerScore += 1;
   } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
     console.log('You Lose!');
+    gameOutcomeElement.textContent = 'You Lose!';
     computerScore += 1;
   } else if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
     console.log('You Lose!');
+    gameOutcomeElement.textContent = 'You Lose!';
     computerScore += 1;
   } else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
     console.log('You Lose!');
+    gameOutcomeElement.textContent = 'You Lose!';
     computerScore += 1;
   } else {
     console.log('You Draw');
+    gameOutcomeElement.textContent = 'You Draw!';
   }
   console.info('Player:', playerScore);
   console.info('computer:', computerScore);
@@ -47,13 +54,16 @@ const calculateGame = (computerSelection, playerSelection) => {
 const checkWinner = () => {
   if (playerScore >= 5) {
     console.log('Game End, You win');
+    gameOutcomeElement.textContent = 'Game End, You win';
   } else if (computerScore >= 5) {
     console.log('Game End, You Lose. Computer Win');
+    gameOutcomeElement.textContent = 'Game End, You Lose. Computer Win';
   }
 };
 
 const playGame = (event) => {
   if (playerScore >= 5 || computerScore >= 5) {
+    gameOutcomeElement.textContent = 'Click the New Game Button to Restart Game';
     return console.log('Click the New Game Button to Restart Game');
   }
   const playerSelection = event.target.textContent;
@@ -75,6 +85,9 @@ scissorsButton.addEventListener('click', playGame);
 const startNewGame = () => {
   playerScore = 0;
   computerScore = 0;
+  gameOutcomeElement.textContent = '';
+  playerChoiceElement.textContent = '';
+  computerChoiceElement.textContent = '';
 };
 
 newGameButton.addEventListener('click', startNewGame);
