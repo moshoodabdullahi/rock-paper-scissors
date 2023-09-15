b733d71f8586804474ae5a57ff049d9c65fe878e
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-09-15 10:36:00 +0100

    feat: add new design for rock paper scissors

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
+
+const playGame = document.querySelector('#play-game');
+const gameResult = document.querySelector('#game-result');
+
+console.log('Hello World');
