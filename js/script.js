de8c55446fb7490ee2c5dd8df9ad8e24187ae0d2
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-09-17 17:41:13 +0100

    feat: make project display score of player and computer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,73 @@
+let playerScore = 0;
+let computerScore = 0;
+
 const rockButton = document.querySelector('#rock-button');
 const paperButton = document.querySelector('#paper-button');
 const scissorsButton = document.querySelector('#scissors-button');
 
-const playGame = document.querySelector('#play-game');
-const gameResult = document.querySelector('#game-result');
+const gameScreenElement = document.querySelector('#game-screen-element');
+const resultScreenElement = document.querySelector('#result-screen-element');
 
-console.log('Hello World');
+const playerScoreElement = document.querySelector('#player-score-element');
+const computerScoreElement = document.querySelector('#computer-score-element');
+
+const playAgainBtn = document.querySelector('#play-again-button');
+
+const getComputerChoice = () => {
+  const choice = ['rock', 'paper', 'scissors'];
+  const random = Math.floor(Math.random() * choice.length);
+  return choice[random];
+};
+
+const calculateGame = (computerSelection, playerSelection) => {
+  if (computerSelection === 'rock' && playerSelection === 'paper') {
+    console.log('You Win!');
+    playerScore += 1;
+  } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+    console.log('You Win!');
+    playerScore += 1;
+  } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
+    console.log('You Win!');
+    playerScore += 1;
+  } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+    console.log('You Lose!');
+    computerScore += 1;
+  } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+    console.log('You Lose!');
+    computerScore += 1;
+  } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+    console.log('You Lose!');
+    computerScore += 1;
+  } else {
+    console.log('You Draw');
+  }
+  console.info('Player:', playerScore);
+  console.info('computer:', computerScore);
+  playerScoreElement.textContent = playerScore;
+  computerScoreElement.textContent = computerScore;
+};
+
+const playRound = (event) => {
+  const { value } = event.currentTarget;
+  console.log(value);
+  const playerSelection = event.currentTarget.value;
+  const computerSelection = getComputerChoice();
+  console.info('Player Chooses:', playerSelection);
+  console.info('Computer Chooses:', computerSelection);
+  calculateGame(computerSelection, playerSelection);
+  gameScreenElement.classList.replace('d-flex', 'd-none');
+  resultScreenElement.classList.replace('d-none', 'd-flex');
+};
+
+const playAgain = () => {
+  gameScreenElement.classList.replace('d-none', 'd-flex');
+  resultScreenElement.classList.replace('d-flex', 'd-none');
+};
+
+rockButton.addEventListener('click', playRound);
+
+paperButton.addEventListener('click', playRound);
+
+scissorsButton.addEventListener('click', playRound);
+
+playAgainBtn.addEventListener('click', playAgain);
